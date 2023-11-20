d50c4641d22ca5f3e55ddbdbc452b74e832c1d6f
awoliop <awoliioop@gmail.com>
2023-11-20 12:43:24 +0300

    Shuffle The Array

--- shuffleTheArray.js
+++ shuffleTheArray.js
@@ -6,8 +6,7 @@
 var shuffle = function (nums, n) {
   let arr1 = nums.slice(0, n);
   let arr2 = nums.slice(n, nums.length);
-  console.log(arr1);
-  console.log(arr2);
+
   let shuffled = [];
   for (let i = 0; i < arr1.length; i++) {
     shuffled.push(arr1[i]);
